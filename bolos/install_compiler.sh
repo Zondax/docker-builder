404f3e51e4b3b84a7d4886965afc171ab80326dd
Juan Leni <lenijuan@gmail.com>
2020-08-05 00:02:56 +0200

    check downloads / hashes

--- bolos/install_compiler.sh
+++ bolos/install_compiler.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+set -e
 
 DEST=/opt/bolos
 
+rm -rf ${DEST} || true
 mkdir -p ${DEST}
 cd ${DEST}
 
-wget -q https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q1-update/+download/gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2
-wget -q http://releases.llvm.org/7.0.0/clang+llvm-7.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+wget https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q1-update/+download/gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2
+wget https://releases.llvm.org/7.0.0/clang+llvm-7.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 
+echo "69b85c833cd28ea04ce34002464f10a6ad9656dd2bba0f7133536a9927c660d2  clang+llvm-7.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz" | sha256sum --check
+echo "217850b0f3297014e8e52010aa52da0a83a073ddec4dc49b1a747458c5d6a223  gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2" | sha256sum --check
+
+echo "decompress gcc..."
 tar xfj gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2
-tar xfv clang+llvm-7.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+echo "decompress clang..."
+tar xf clang+llvm-7.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 
 mv clang+llvm-7.0.0-x86_64-linux-gnu-ubuntu-16.04 clang-arm-fropi
 chmod 757 -R clang-arm-fropi/
